a46f55782383578e51ae480ecefd2e09b73c9eee
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-07-07 14:13:35 -0300

    Ordenamiento de datos Se agregaron funciones de ordenamiento y comparacion

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+console.clear;
+
 let tamanoArreglo: number = 0;
 tamanoArreglo = Number(
   prompt("Indique  cantidad  de persoNAS a cargar Vector : ")
@@ -11,7 +13,13 @@ function getRndInteger(min, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function cargarVectorManual(v: string[]) {
+function cargarVectorManualStr(v: string[]) {
+  for (let indice = 0; indice < v.length; indice++) {
+    v[indice] = prompt("Ingrese el Nombre de la posicion ", indice, ":");
+  }
+}
+
+function cargarVectorManualInt(v: number[]) {
   for (let indice = 0; indice < v.length; indice++) {
     v[indice] = prompt("Ingrese el Nombre de la posicion ", indice, ":");
   }
@@ -24,27 +32,27 @@ function cargarVector(v: number[], min: number, max: number) {
   }
 }
 
-cargarVectorManual(nombre);
-cargarVector(anio, 1981, 2000);
-cargarVector(altura, 156, 200);
+cargarVectorManualStr(nombre);
+cargarVectorManualInt(anio);
+cargarVectorManualInt(altura);
 
 function intercambiar(arreglo: number[], i: number, j: number) {
   let aux: number;
   aux = arreglo[i];
   arreglo[i] = arreglo[j];
-  arreglo[j] = arreglo[i];
+  arreglo[j] = aux;
 }
 
 function intercambiarStr(arreglo: string[], i: number, j: number) {
   let aux: string;
   aux = arreglo[i];
   arreglo[i] = arreglo[j];
-  arreglo[j] = arreglo[i];
+  arreglo[j] = aux;
 }
 
-function comparar(arreglo: number[], i: number, j: number): number {
+function compararUnArray(arreglo: number[], i: number, j: number): number {
   let comparacion: number;
-  if (arreglo[i] === arreglo[j]) {
+  if (arreglo[i] == arreglo[j]) {
     comparacion = 0;
   } else if (arreglo[i] < arreglo[j]) {
     comparacion = -1;
@@ -54,12 +62,29 @@ function comparar(arreglo: number[], i: number, j: number): number {
   return comparacion;
 }
 
-function burbujaAnio(arreglo: number[], cantidad: number) {
+function comparar(edad: number[], altura: number[], i: number, j: number) {
+  let comparacion: number;
+  if (edad[i] == edad[j]) {
+    comparacion = compararUnArray(altura, i, j);
+  } else if (edad[i] < edad[j]) {
+    comparacion = -1;
+  } else {
+    comparacion = 1;
+  }
+  return comparacion;
+}
+
+function ordenarBurbuja(
+  edad: number[],
+  altura: number[],
+  nombre: string[],
+  cantidad: number
+) {
   // Algoritmo de burbujaa
   for (let i: number = 0; i < cantidad - 1; i++) {
     for (let j: number = 0; j < cantidad - 1 - i; j++) {
-      if (arreglo[j] > arreglo[j + 1]) {
-        intercambiar(arreglo, j, j + 1);
+      if (comparar(edad, altura, j, j + 1) == 1) {
+        intercambiar(edad, j, j + 1);
         intercambiar(altura, j, j + 1);
         intercambiarStr(nombre, j, j + 1);
       }
@@ -67,21 +92,12 @@ function burbujaAnio(arreglo: number[], cantidad: number) {
   }
 }
 
-function burbuja(
-  arreglo: number[],
-  cantidad: number,
-  posicionI: number,
-  posicionJ: number,
-  anio: number
-) {
-  // Algoritmo de burbujaa
-  for (let i: number = posicionI; i < cantidad - 1; i++) {
-    for (let j: number = posicionJ; j < cantidad - 1 - i; j++) {
-      if (arreglo[j] > arreglo[j + 1]) {
-        intercambiar(arreglo, j, j + 1);
-        intercambiar(anio, j, j + 1);
-        intercambiarStr(nombre, j, j + 1);
-      }
-    }
-  }
-}
+console.log("DATOS CARGADOS");
+console.log(nombre);
+console.log(anio);
+console.log(altura);
+console.log("DATOS ORDENADOS");
+ordenarBurbuja(anio, altura, nombre, tamanoArreglo);
+console.log(nombre);
+console.log(anio);
+console.log(altura);
